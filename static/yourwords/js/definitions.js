6f0a7bf52e31dc398fe2f03e907107a061a6b1ef
Adam Guła <twentiethsite@linux.pl>
2017-09-29 21:45:00 +0200

    zamiana starej funkcji showAndHideModal na nową hideModal + wyświetlenie odpowiedzi w przypadku zatwierdzenia pustegopola tekstowego

--- static/yourwords/js/definitions.js
+++ static/yourwords/js/definitions.js
@@ -1,75 +1,51 @@
 
-	function createRandomColouredList(arr, ul_class, is_horizontal)
+function createRandomColouredList(arr, ul_class, is_horizontal)
+{
+	var c
+	function setRandomInt(min, max)
 	{
-		var c
-		function setRandomInt(min, max)
-		{
-			min = Math.ceil(min)
-			max = Math.floor(max)
-			return Math.floor(Math.random()*(max-min+1))+min
-		}
-		$('.' + ul_class).css({
-			'display': 'block',
-			'list-style-type': 'none',
-			'text-align': 'center',
-			'padding': '0',
-			'margin': '0 3px 0 5px'
-		})
-		if(is_horizontal)
-		{
-			$('.' + ul_class + ' li').css({
-				'display': 'inline-block'
-			})
-		}
-		$('.' + ul_class + ' li a').css({
-			'text-decoration': 'none',
-			'display': 'block',
-			'padding': '10px',
-			'color': 'black'
-		})
-		$('.material-icons').css({
-			'padding': '0',
-			'margin': '0'
-		})
-		$('.' + ul_class + ' li a').not('.disabled').mouseenter(function(){
-			c = 'w3-hover-' + arr[setRandomInt(0, arr.length-1)]
-			$(this).addClass(c)
-		}).mouseleave(function(){
-			$(this).removeClass(c)
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random()*(max-min+1))+min
+	}
+	$('.' + ul_class).css({
+		'display': 'block',
+		'list-style-type': 'none',
+		'text-align': 'center',
+		'padding': '0',
+		'margin': '0 3px 0 5px'
+	})
+	if(is_horizontal)
+	{
+		$('.' + ul_class + ' li').css({
+			'display': 'inline-block'
 		})
 	}
-
-	function showAndHideModal(type, content)
-	{
-		$('.modal-header').removeClass('w3-blue').removeClass('w3-orange').removeClass('w3-red')
-
-		if(type == 'error')
-		{
-			$('.modal-header, .ok').addClass('w3-red')
-		}
-		if(type == 'warning')
-		{
-			$('.modal-header, .ok').addClass('w3-orange')
-		}
-		if(type == 'info')
-		{
-			$('.modal-header, .ok').addClass('w3-blue')
-		}
-		$('.modal-content').html(content)
-		$('.modal-base').show()
-		$('.modal-base').find('.modal-header, .ok').click(function() {
-			$('.modal-content').html('')
-			$('.modal-base').hide()
-		})
-	}
+	$('.' + ul_class + ' li a').css({
+		'text-decoration': 'none',
+		'display': 'block',
+		'padding': '10px',
+		'color': 'black'
+	})
+	$('.material-icons').css({
+		'padding': '0',
+		'margin': '0'
+	})
+	$('.' + ul_class + ' li a').not('.disabled').mouseenter(function(){
+		c = 'w3-hover-' + arr[setRandomInt(0, arr.length-1)]
+		$(this).addClass(c)
+	}).mouseleave(function(){
+		$(this).removeClass(c)
+	})
+}
 
 function showModal(txt, options = {}) {
 	var defs = {
 		modal: $('.modal-base'),
 		ok: $('.ok'),
 		cancel: $('.cancel'),
-		header: $('.modal-header'),
-		footer: $('.modal-footer'),
+		header: $('.modal-base-header'),
+		footer: $('.modal-base-footer'),
 		txt: txt.trim(),
 		hideModal: function() {
 			this.modal.hide();
