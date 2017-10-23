7e69cd59c8a4e8209790e6889e9272318888aca1
Adam Guła <twentiethsite@linux.pl>
2017-10-23 21:59:59 +0200

    koniec, działa

--- administration/models.py
+++ administration/models.py
@@ -1,50 +1,16 @@
 from django.db import models
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-from django.dispatch import receiver
 
 
 class Log(models.Model):
     ACTION_CHOICES = (
         ('1', 'The user has logged in.'),
         ('2', 'The user has logged out.'),
+        ('3', 'The user has registered.'),
     )
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     action = models.CharField(max_length=255, choices=ACTION_CHOICES)
+    ip = models.GenericIPAddressField(default=None, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return '{} | {} | {}'.format(self.user.username, self.action, self.created_at)
-
-    @classmethod
-    def apply(cls, user, action):
-        # logModel = cls(user=user, action=action)
-        # try:
-        #     logModel.save()
-        #     return True
-        # except IntegrityError:
-        #     return False
-        # except ValueError:
-        #     return False
-        pass
-
-    @receiver(user_logged_in)
-    def user_has_been_logged_in(sender, user, request, **kwargs):
-        logModel = Log(user=user, action='1')
-        try:
-            logModel.save()
-            return True
-        except IntegrityError:
-            return False
-        except ValueError:
-            return False
-
-    @receiver(user_logged_out)
-    def user_has_been_logged_out(sender, user, request, **kwargs):
-        logModel = Log(user=user, action='2')
-        try:
-            logModel.save()
-            return True
-        except IntegrityError:
-            return False
-        except ValueError:
-            return False
